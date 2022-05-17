01ed182d351a38ebd3095633a6b7c6ec07855126
Olexandr Zakordonets <wpcsani@gmail.com>
2022-05-18 00:16:56 +0300

    Minor fix

--- UploadCSV/datasets/serializers.py
+++ UploadCSV/datasets/serializers.py
@@ -5,23 +5,8 @@ from .models import DataSet, DataSetExample
 from . import DATASET_SCHEMA
 
 
-class DataSetSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = DataSet
-        fields = ['name', 'json_schema']
-
-    def validate_json_schema(self, value):
-        try:
-            jsonschema.validate(value, DATASET_SCHEMA)
-        except jsonschema.ValidationError:
-            raise serializers.ValidationError('Schema not valid')
-        return value
-
-
 class GeneratorSerializer(serializers.Serializer):
     row_number = serializers.IntegerField()
-
-
 
 
 class DataSetExampleSerializer(serializers.ModelSerializer):
@@ -32,4 +17,19 @@ class DataSetExampleSerializer(serializers.ModelSerializer):
     class Meta:
         model = DataSetExample
         fields = ['file', 'process_status']
+
+
+class DataSetSerializer(serializers.ModelSerializer):
+    datasetexample_set = DataSetExampleSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = DataSet
+        fields = ['name', 'json_schema', 'datasetexample_set']
+
+    def validate_json_schema(self, value):
+        try:
+            jsonschema.validate(value, DATASET_SCHEMA)
+        except jsonschema.ValidationError:
+            raise serializers.ValidationError('Schema not valid')
+        return value
 
