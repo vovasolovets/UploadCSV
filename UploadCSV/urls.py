5f32ceb47130052ae28173c3c0b485261ca91528
Olexandr Zakordonets <wpcsani@gmail.com>
2022-05-17 21:33:35 +0300

    Add views

--- UploadCSV/urls.py
+++ UploadCSV/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
+from .datasets.urls import router
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path(r'^login/$', auth_views.LoginView.as_view(), name='login'),
+    path(r'^logout/$', auth_views.LogoutView.as_view(), name='logout'),
+    path('upload/', include(router.urls))
 ]
