93b538c7673e73b430ebf5755b5190e483673ad0
Olexandr Zakordonets <wpcsani@gmail.com>
2022-05-19 20:55:50 +0300

    Minor fix

--- UploadCSV/settings.py
+++ UploadCSV/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-t4m)aea)^vobse2p^=s225y^&^0r0@uc18-0#740pgex3!hq5='
+#SECRET_KEY = 'django-insecure-t4m)aea)^vobse2p^=s225y^&^0r0@uc18-0#740pgex3!hq5='
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
