7bbc9d0a9e3a17d6c86a8f568555fcfce33e919a
Olexandr Zakordonets <wpcsani@gmail.com>
2022-05-26 20:07:16 +0300

    Minor fix

--- UploadCSV/settings.py
+++ UploadCSV/settings.py
@@ -126,6 +126,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'build/static')
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
@@ -186,3 +192,4 @@ CORS_ORIGIN_WHITELIST = (
 CSRF_TRUSTED_ORIGINS = [
     'http://localhost:3000'
 ]
+
